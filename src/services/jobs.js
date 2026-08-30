9df9fd5b484a7d3f7fd4b021a441ade5b1e972cf
Ali <Alialaliwat21@gmail.com>
2026-08-30 14:33:37 +0300

    jobs.js services fetching file created

--- src/services/jobs.js
+++ src/services/jobs.js
@@ -2,8 +2,21 @@ const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}`
 
 const index = async ()=>{
     try {
-        const
-    } catch (error) {
+        const res = await fetch(`${BASE_URL}/api/v1/jobs`)
+
+        const data = await res.json()
+
+        if (!res.ok) {
+            throw new Error(data.message)
+        }
+
+        return data
+    } catch (err) {
+        throw new Error(err.message)
         
     }
+}
+
+export {
+    index
 }
