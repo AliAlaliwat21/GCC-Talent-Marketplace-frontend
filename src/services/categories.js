8161a9d6bf664662c43fd8bf6c975deb2c9a34a2
Ali <Alialaliwat21@gmail.com>
2026-08-31 10:10:44 +0300

    added create function to jobs.js and created and categories in services

---
+++ src/services/categories.js
@@ -0,0 +1,18 @@
+const BASE_URL = `${import.VITE_BACK_END_SERVER_URL}/api/v1/categories`
+
+const index = async ()=>{
+    const res = await fetch(BASE_URL)
+    const data = await res.json()
+
+    if(!res.ok){
+        throw new Error(
+            data.message || "Failed to fetch categories"
+        )
+    }
+    return data
+    
+}
+
+export {
+    index
+}
