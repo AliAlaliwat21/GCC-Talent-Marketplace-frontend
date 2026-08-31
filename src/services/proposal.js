cbeeefdcca8f7ab99fe390d0e2ccf57d4cbb237b
HALI12_hat <hussainaliabdulrasool004@gmail.com>
2026-08-31 11:00:00 +0300

    made proposal service fetched from backend, made proposalform component a child in jobdetails

---
+++ src/services/proposal.js
@@ -0,0 +1,25 @@
+const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}`
+
+const create = async (jobId, proposalData)=>{
+    try {
+        const res = await fetch(`${BASE_URL}/api/v1/jobs/${jobId}/proposals`, {
+            method: 'POST',
+            headers: { 'ContentType': 'application/json', Authorization: `Bearer ${localStorage.getItem('token')}` },
+            body: JSON.stringify(proposalData)
+        })
+
+        const data = await res.json()
+
+        if (!res.ok){
+            throw new Error(data.error || data.message)
+        }
+
+        return data
+    } catch (error) {
+        throw new Error(error.message)
+    }
+}
+
+export {
+    create
+}
