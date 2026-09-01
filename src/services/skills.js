3f97607ee5c2130ee01f5ce3bc5340ad7d5f9d7e
Jassiiimm <owainatijassim@gmail.com>
2026-09-01 22:19:48 +0300

    Admin can create new skills for clients and freelancers

--- src/services/skills.js
+++ src/services/skills.js
@@ -1,4 +1,5 @@
 const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/api/v1/skills`
+const ADMIN_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/api/v1/admin/skills`
 
 const index = async () => {
     const res = await fetch(BASE_URL)
@@ -11,6 +12,24 @@ const index = async () => {
     return data
 }
 
+const create = async function (skillData) {
+    const token = localStorage.getItem("token")
+    const res = await fetch(ADMIN_URL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify(skillData)
+    })
+    const data = await res.json()
+    if (!res.ok) {
+        throw new Error(data.message)
+    }
+    return data
+}
+
 export {
-    index
+    index,
+    create
 }
