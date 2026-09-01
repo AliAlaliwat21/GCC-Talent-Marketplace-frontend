c2c01aa577afd904de7ce8ce5b84b987f02a3978
Jassiiimm <owainatijassim@gmail.com>
2026-09-01 14:59:37 +0300

    Added admin statistics request

---
+++ src/services/admin.js
@@ -0,0 +1,17 @@
+const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/api/v1/admin`
+const getStats = async function () {
+    const token = localStorage.getItem("token")
+    const res = await fetch(`${BASE_URL}/stats`, {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    })
+    const data = await res.json()
+    if (!res.ok) {
+        throw new Error(data.message)
+    }
+    return data
+}
+export {
+    getStats
+}
