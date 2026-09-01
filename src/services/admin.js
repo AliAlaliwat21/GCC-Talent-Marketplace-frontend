2b520e47be8b5e778e8c14cf4b3b219543607b91
Jassiiimm <owainatijassim@gmail.com>
2026-09-01 16:19:34 +0300

    Admin can create categories now

--- src/services/admin.js
+++ src/services/admin.js
@@ -1,4 +1,5 @@
 const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/api/v1/admin`
+
 const getStats = async function () {
     const token = localStorage.getItem("token")
     const res = await fetch(`${BASE_URL}/stats`, {
